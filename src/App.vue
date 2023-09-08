63c9576255c9ff66e7ba711a2687b6819ac4f266
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-09-08 15:24:43 +0200

    First-commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+// IMPORTO I COMPONENTS DENTRO L'APP.VUE
+
+import AppFooter from "./components/AppFooter.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
+// ESPORTO  I COMPONENTS DENTRO L'APP.VUE
+
+export default {
+  components: { AppFooter, AppHeader, AppMain },
+};
+</script>
+
+<template>
+  <h1>
+    <AppHeader />
+  </h1>
+</template>
+<style></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h1>ciao</h1>
+</template>
+<style></style>
